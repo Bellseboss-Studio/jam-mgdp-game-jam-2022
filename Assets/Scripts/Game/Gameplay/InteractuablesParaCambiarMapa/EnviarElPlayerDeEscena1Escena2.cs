14630fc8f32235b9850f10aa07a291fb17a96305
Luis Plata <www.luisplata@gmail.com>
2022-08-17 22:55:32 -0500

    update

---
+++ Assets/Scripts/Game/Gameplay/InteractuablesParaCambiarMapa/EnviarElPlayerDeEscena1Escena2.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnviarElPlayerDeEscena1Escena2 : InteractiveObjectFather
+{
+    [SerializeField] private int sceneToLoad;
+    protected override void ActionEventCustom()
+    {
+        
+    }
+}
