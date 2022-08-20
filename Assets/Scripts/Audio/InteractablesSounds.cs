ae26d8d7697005feb2b53faf13ff038d2de687d5
IvanDBeltran <66095364+IvanDBeltran@users.noreply.github.com>
2022-08-20 00:34:00 -0400

    Interactables Audio

---
+++ Assets/Scripts/Audio/InteractablesSounds.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections;
+using UnityEngine;
+using System.Collections.Generic;
+using SystemOfExtras;
+
+namespace GameAudio
+{
+    public class InteractablesSounds : MonoBehaviour
+    {
+        [SerializeField] private Transform[] m_SoundObjects;
+        private Dictionary<string, GameObject> m_AudioObjectsDictionary = new Dictionary<string, GameObject>();
+
+
+        private void Start()
+        {
+            ServiceLocator.Instance.RegisterService(this);
+            AddItemsToDictionary();
+        }
+
+        public void PlaySound(string name)
+        {
+            StartCoroutine(PlaySoundCorroutine(name));
+        }
+        
+        IEnumerator PlaySoundCorroutine(string name)
+        {
+            GameObject go = m_AudioObjectsDictionary[name];
+            go.SetActive(true);
+            yield return new WaitForSeconds(go.GetComponent<AudioSource>().clip.length);
+            go.SetActive(false);
+        }
+        
+        private void AddItemsToDictionary()
+        {
+            m_SoundObjects = new Transform[transform.childCount];
+            for (int i = 0; i < transform.childCount; i++)
+            {
+                m_SoundObjects[i] = transform.GetChild(i);
+            }
+            foreach (Transform t in m_SoundObjects)
+            {
+                m_AudioObjectsDictionary.Add(t.gameObject.name, t.gameObject);
+            }
+        }
+    }
+}
