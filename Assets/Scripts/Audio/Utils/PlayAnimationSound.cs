26597593534e94ac6602859549f39b47fb696e84
IvanDBeltran <66095364+IvanDBeltran@users.noreply.github.com>
2022-08-20 15:38:08 -0400

    Door sounds

--- Assets/Scripts/Audio/Utils/PlayAnimationSound.cs
+++ Assets/Scripts/Audio/Utils/PlayAnimationSound.cs
@@ -1,29 +1,30 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using GameAudio;
 using UnityEngine;
 
 
-public class PlayAnimationSound : MonoBehaviour, ICheckDependencies
+namespace GameAudio
 {
-    [SerializeField] private AudioClip m_Sound = null;
-    [SerializeField] private AudioSource m_As = null;
-    
-    private void Start()
+    public class PlayAnimationSound : MonoBehaviour, ICheckDependencies
     {
-       CheckDependencies();
-    }
+        [SerializeField] private AudioClip m_Sound = null;
+        [SerializeField] private AudioSource m_As = null;
 
-    public void PlaySound()
-    {
-        m_As.PlayOneShot(m_Sound);
-    }
-    public void CheckDependencies()
-    {
-        if (m_As == null)
+        private void Start()
         {
-            m_As = GetComponentInChildren<AudioSource>();
+            CheckDependencies();
+        }
+        public void PlaySound()
+        {
+            m_As.PlayOneShot(m_Sound);
+        }
+
+        public void CheckDependencies()
+        {
+            if (m_As == null)
+            {
+                m_As = GetComponentInChildren<AudioSource>();
+            }
         }
     }
 }
