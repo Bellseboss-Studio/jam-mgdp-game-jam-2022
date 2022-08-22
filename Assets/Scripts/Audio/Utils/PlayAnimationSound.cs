9ac23f41b73a5694eeef1a180de0361627d04693
IvanDBeltran <66095364+IvanDBeltran@users.noreply.github.com>
2022-08-21 22:28:49 -0400

    Updated distance based audio player for animations
    I updated this script to calculate the distance between the source and the audiolistener and deactivate/activate the AudioSource accordingly.

--- Assets/Scripts/Audio/Utils/PlayAnimationSound.cs
+++ Assets/Scripts/Audio/Utils/PlayAnimationSound.cs
@@ -10,13 +10,41 @@ namespace GameAudio
         [SerializeField] private AudioClip m_Sound = null;
         [SerializeField] private AudioSource m_As = null;
 
+        [Header("Distance to Audio Listener")]
+        [SerializeField] private bool m_CheckForDistance = true;
+        [SerializeField] private float m_Magnitude;
+        [SerializeField] private float m_MinDistance;
+        
+        
+        [Tooltip("Transform of the AudioListener and the GameObject with the audio source")]
+        [SerializeField] private Transform m_Target;
+        [SerializeField] private GameObject m_Emitter;
         private void Start()
         {
             CheckDependencies();
+            
         }
         public void PlaySound()
         {
-            m_As.PlayOneShot(m_Sound);
+            if (m_CheckForDistance)
+            {
+                m_Magnitude = Vector3.Magnitude(m_Target.position - this.gameObject.transform.position);
+                if(m_Magnitude < m_MinDistance)
+                {
+                    m_Emitter.SetActive(true);
+                    m_As.PlayOneShot(m_Sound);
+                }
+                else
+                {  
+                    m_Emitter.SetActive(false);
+                }
+            }
+            else
+            {
+                m_Emitter.SetActive(true);
+                m_As.PlayOneShot(m_Sound);
+            }
+            
         }
 
         public void CheckDependencies()
@@ -25,6 +53,8 @@ namespace GameAudio
             {
                 m_As = GetComponentInChildren<AudioSource>();
             }
+            m_Target = FindObjectOfType<AudioListener>().transform;
+            m_Emitter = m_As.gameObject;
         }
     }
 }
