711d8c8ca570a919169b321b0a68e9d66b290787
PeryLoth <www.luisplata@gmail.com>
2023-11-14 15:27:44 -0500

    update systems and reorganice elements

--- Assets/Scripts/Audio/Utils/QuitGame.cs
+++ Assets/Scripts/Audio/Utils/QuitGame.cs
@@ -1,9 +1,13 @@
 using System.Collections;
+using GameAudio;
+using SystemOfExtras;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class QuitGame : MonoBehaviour
 {
     [SerializeField] private float m_delayTime = 0.5f;
+    [SerializeField] private bool isReset;
     public void OnQuitGameClicked()
     {
         StartCoroutine(nameof(TerminateGameSequence));
@@ -12,9 +16,19 @@ public class QuitGame : MonoBehaviour
     IEnumerator TerminateGameSequence()
     {
         yield return new WaitForSeconds(m_delayTime);
-        Application.Quit();
-        #if UNITY_EDITOR
-        UnityEditor.EditorApplication.isPlaying = false;
-        #endif
+        if (isReset)
+        {
+            //reset game
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            ServiceLocator.Instance.GetService<UIControl>().HideUI();
+            ServiceLocator.Instance.GetService<MusicSystem>().UnpauseMixer();
+        }
+        else
+        {
+            Application.Quit();
+#if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+#endif
+        }
     }
 }
---
+++ Assets/Scripts/SystemOfExtras/TurnOffTv.cs
@@ -0,0 +1,14 @@
+﻿
+using UnityEngine;
+
+public class TurnOffTv : InteractiveObjectFather
+{
+    [SerializeField] private GameObject[] screem;
+    protected override void ActionEventCustom()
+    {
+        foreach (var item in screem)
+        {
+            item.SetActive(false);
+        }
+    }
+}
